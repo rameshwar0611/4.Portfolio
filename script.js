62be4eb2991ddf6c2f330324c45b14932192339f
rameshwar0611 <rameshwardangi0611@gmail.com>
2024-12-29 23:21:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Backend Developer", "DSA Master"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true
+});
